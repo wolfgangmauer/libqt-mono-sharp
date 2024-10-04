942bbc1df50f912c6e3f4880144925cab8b33bd4
Wolfgang Mauer <wolfgang.mauer@kabelmail.de>
2024-10-04 04:39:08 +0200

    Update QGlueLabel.h

--- QGlueLabel.h
+++ QGlueLabel.h
@@ -6,10 +6,12 @@ class GlueLabel : public QLabel
 {
 #define BaseClass QLabel
 #include "internalevents.h"
+Q_OBJECT
 public:
     GlueLabel(MonoObject* thisObject, QWidget *parent=0, Qt::WindowFlags f=0);
     GlueLabel(MonoObject* thisObject, char* text, QWidget *parent=0, Qt::WindowFlags f=0);
     ~GlueLabel();
+    void updateTextValue(char* text);
 private:
     guint32 _thisObject;
 };
