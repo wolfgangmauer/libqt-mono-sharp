9b76602bbb00e41e64ac3c98e93065149d387b64
Wolfgang Mauer <wolfgang.mauer@kabelmail.de>
2024-10-04 03:19:15 +0200

    Update QGlueProgressBar.cpp

--- QGlueProgressBar.cpp
+++ QGlueProgressBar.cpp
@@ -5,11 +5,6 @@ GlueProgressBar::GlueProgressBar(MonoObject* thisObject, QWidget* parent)
 {
 	_thisObject = mono_gchandle_new(thisObject, TRUE);
 	_nameSpace = mono_class_get_namespace(mono_object_get_class (mono_gchandle_get_target(_thisObject)));
-	connect(this, SIGNAL(updateValue(int)), this, SLOT(setValue(int)));
-}
-void GlueProgressBar::updateProgressValue(int value)
-{
-	emit updateValue(value);
 }
 
 GlueProgressBar::~GlueProgressBar()
