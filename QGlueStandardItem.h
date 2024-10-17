b729a276c46e63b09cc0441345fe2ac63c28d7fc
Wolfgang Mauer <wolfgang.mauer@kabelmail.de>
2024-10-17 14:44:21 +0200

    Update QGlueStandardItem.h

--- QGlueStandardItem.h
+++ QGlueStandardItem.h
@@ -1,1 +1,12 @@
+﻿#pragma once
+#include <QStandardItem>
+#include "QGlueWidget.h"
 
+class GlueStandardItem : public QStandardItem
+{
+public:
+    GlueStandardItem(MonoObject* thisObject, GlueObject* parent=0);
+
+private:
+    guint32 _thisObject;
+};
