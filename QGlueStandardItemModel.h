201dee888c6c4ffdb5ff874339ee381316c397d7
Wolfgang Mauer <wolfgang.mauer@kabelmail.de>
2024-10-20 17:48:15 +0200

    Update QGlueStandardItemModel.h

--- QGlueStandardItemModel.h
+++ QGlueStandardItemModel.h
@@ -7,8 +7,6 @@ class GlueStandardItemModel : public QStandardItemModel
 public:
     GlueStandardItemModel(MonoObject* thisObject, GlueObject* parent=0);
     GlueStandardItemModel(MonoObject* thisObject, int rows, int cols, GlueObject* parent=0);
-public slots:
-    void onSelectioChanged(const QItemSelection &, const QItemSelection &);
 private:
     guint32 _thisObject;
 };
