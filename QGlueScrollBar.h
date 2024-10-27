f7984dbf16f8f68c2f0176d0fdb82a290e01ee7a
Wolfgang Mauer <wolfgang.mauer@kabelmail.de>
2024-10-27 02:18:02 +0200

    Update and rename QGlueScrollbar.h to QGlueScrollBar.h

---
+++ QGlueScrollBar.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+#include <QScrollBar>
+
+class GlueScrollBar : public QScrollBar
+{
+public:
+    GlueScrollBar(MonoObject* thisObject, QWidget *parent=0);
+    ~GlueScrollBar();
+private:
+    guint32 _thisObject;
+};
