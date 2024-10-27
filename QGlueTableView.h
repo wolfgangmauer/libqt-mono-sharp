81c56f9606e766e3032bb3ad0d04cd12f8d629c6
Wolfgang Mauer <wolfgang.mauer@kabelmail.de>
2024-10-27 04:50:27 +0100

    Update QGlueTableView.h

--- QGlueTableView.h
+++ QGlueTableView.h
@@ -10,7 +10,7 @@ class GlueTableView : public QTableView
 public:
     explicit GlueTableView(MonoObject* thisObject, QWidget *parent=0);
     ~GlueTableView();
-    QModelIndex* move_cursor(GlueTableView* tableView, QAbstractItemView::CursorAction cursorAction);
+    QModelIndex* move_cursor(QAbstractItemView::CursorAction cursorAction);
 protected:
 	void currentChanged(const QModelIndex &current, const QModelIndex &previous) override;
 private:
