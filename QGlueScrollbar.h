08edbd27b6e169941309daf7c0248fbdc1a0625b
Wolfgang Mauer <wolfgang.mauer@kabelmail.de>
2024-10-27 02:15:15 +0200

    Create QGlueScrollbar.h

---
+++ QGlueScrollbar.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+#include <QScrollbar>
+
+class GlueScrollbar : public QScrollbar
+{
+public:
+    GlueScrollbar(MonoObject* thisObject, QWidget *parent=0);
+    ~GlueScrollbar();
+private:
+    guint32 _thisObject;
+};
