2120d15319060843307366bd6fe3cc51e0279be3
Wolfgang Mauer <wolfgang.mauer@kabelmail.de>
2024-10-28 15:25:27 +0100

    Create QGlueTableWidgetItem.cpp

---
+++ QGlueTableWidgetItem.cpp
@@ -0,0 +1,11 @@
+﻿#include "QGlueTableWidgetItem.h"
+
+GlueTableWidgetItem::GlueTableWidgetItem(MonoObject* thisObject, char* text)
+	: QTableWidgetItem(text)
+{
+	_thisObject = mono_gchandle_new(thisObject, TRUE);
+}
+GlueTableWidgetItem::~GlueTableWidgetItem()
+{
+	mono_gchandle_free (_thisObject); 
+}
