685fa977bb121b8bca22c14bc538024847b4dd43
Wolfgang Mauer <wolfgang.mauer@kabelmail.de>
2024-10-28 15:26:44 +0100

    Create QGlueTableWidgetItem.h

---
+++ QGlueTableWidgetItem.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+#include <QTableWidgetItem>
+#include "QGlueWidget.h"
+
+class GlueStandardItem : public QTableWidgetItem
+{
+public:
+    GlueTableWidgetItem(MonoObject* thisObject, char* text);
+    ~GlueTableWidgetItem();
+private:
+    guint32 _thisObject;
+};
