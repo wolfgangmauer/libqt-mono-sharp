e76234bff29127f4780f0c31eb54ea689120c44c
Wolfgang Mauer <wolfgang.mauer@kabelmail.de>
2025-06-13 00:22:03 +0200

    Update QGlueApplication.h

--- QGlueApplication.h
+++ QGlueApplication.h
@@ -14,6 +14,26 @@ class GlueApplication : public QApplication
 public:
 	GlueApplication(MonoObject*, int&, char**);
 	~GlueApplication();
+
+	static void processEvents(QEventLoop::ProcessEventsFlags flags = QEventLoop::AllEvents)
+	{
+		qWarning() << Q_FUNC_INFO << hasGlib;
+		if (!hasGlib) {
+	    		g_main_context_iteration(gContext, false);
+		}
+		QApplication::processEvents(flags);
+	}
+	static void processEvents(QEventLoop::ProcessEventsFlags flags, int maxtime)
+	{
+		qWarning() << Q_FUNC_INFO << hasGlib;
+		if (!hasGlib) {
+	    		g_main_context_iteration(gContext, false);
+		}
+		QApplication::processEvents(flags, maxtime);
+	}
 private:
+	static _GMainContext *gContext;
+	QAbstractEventDispatcher *dispatcher = nullptr;
+	static bool hasGlib;
 	guint32 _thisObject;
 };
